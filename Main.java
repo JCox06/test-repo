5acfac02463c432108044513cba2e272c8ade462
BuildTools <unconfigured@null.spigotmc.org>
2021-04-14 18:59:55 +0100

    Added Main.java
    More indepth explanation

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String args[]) {
+      System.out.println("Hello World, Just testing github from the command line!");
+    }
+
+}
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Main header
 
 A description
+
+
+## SubHeading
+
+Some more text is here.
